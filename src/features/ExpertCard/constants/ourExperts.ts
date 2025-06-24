f20bfaa70387d217eb7eaaaf33e05e2fa959c221
Andranik <andranik.boyajyan@bk.ru>
2025-06-24 19:31:21 +0400

    Home / Our experts

---
+++ src/features/ExpertCard/constants/ourExperts.ts
@@ -0,0 +1,18 @@
+import ourExpert1 from "../../../../public/pngs/ourExpert1.png";
+import ourExpert2 from "../../../../public/pngs/ourExpert2.png";
+import type { ExpertCard } from "../model";
+
+export const ourExperts: ExpertCard[] = [
+  {
+    id: 1,
+    name: "Vahe Ter-Yegishyan, FNP-C",
+    profession: "Cosmetologist-dermatologist",
+    imgUrl: ourExpert1,
+  },
+  {
+    id: 2,
+    name: "Anna Martin ",
+    profession: "Cosmetologist-dermatologist",
+    imgUrl: ourExpert2,
+  },
+];
---
+++ src/features/ExpertCard/model.ts
@@ -0,0 +1,6 @@
+export interface ExpertCard {
+  id: number;
+  name: string;
+  profession: string;
+  imgUrl: string;
+}
