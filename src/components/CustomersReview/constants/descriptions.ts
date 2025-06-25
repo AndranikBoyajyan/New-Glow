ad9daa5a3937b0a5a25ce3257b7b7b426668df4e
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-06-25 23:29:57 +0400

    refactor/home/customer/review/descriptins

---
+++ src/components/CustomersReview/constants/descriptions.ts
@@ -0,0 +1,5 @@
+export const descriptions = {
+  description1: `"From the first consultation to my latest treatment, I've felt so well cared for. The clinics atmosphere is calming, and the results have exceeded my expectations."`,
+  description2: `"I had my first consultation at this clinic a few months ago, and it was the best decision I’ve made for my skin. I’ve always had dry and flaky skin, but after a series of hydrating facials and personalized treatments, my skin is completely transformed. It feels soft, hydrated, and radiant, and I don’t have to worry about the tightness I used to feel every day.`,
+  description3: `"I’ve always been self-conscious about my dark spots, so I decided to visit this clinic based on a friend’s recommendation. From my first consultation. After a few months of consistent treatments, my skin looks brighter, the spots have faded, and I feel so much more confident going out without makeup."`,
+};
