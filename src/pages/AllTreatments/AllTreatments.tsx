f51510037c0dac5a8594f1d1fc8c8e4f84a0281e
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-07-09 20:21:14 +0400

    created / features / component / for / laser / page / 01

--- src/pages/AllTreatments/AllTreatments.tsx
+++ src/pages/AllTreatments/AllTreatments.tsx
@@ -1,12 +1,10 @@
 import AllTreatmentsContentWrapper from "../../components/AllTreatmentsContentWrapper";
 import AllTreatmentsTitleBlock from "../../components/AllTreatmentsTitleBlock";
 import styles from "./AllTreatments.module.css";
-import LaserTreatments from "../../features/LaserTreatments";
 export const AllTreatments = () => {
   return (
     <div className={styles.allTreatmentsPage}>
       <AllTreatmentsTitleBlock />
-      <LaserTreatments />
       <AllTreatmentsContentWrapper />
     </div>
   );
