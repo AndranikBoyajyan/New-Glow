6679df10914dade32b0161bb3e6c3c7af1f623f6
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-07-11 21:34:48 +0400

    created / facial / page

---
+++ src/components/TreatmentsContainers/Facials/constants/cardsInfo.ts
@@ -0,0 +1,92 @@
+export const cardsInfos = [
+  {
+    id: 1,
+    imgUrl: "/pngs/viPeelPrecisionPlus.png",
+    name: "Signature Facial",
+    description:
+      "The Signature Facial is a deep pore cleansing facial, great for all skin types which helps to relax tension in the face, brighten, and improve the overall appearance of your skin. We customize each Signature Facial to help firm, hydrate, reduce roughness, restore its optimum texture, and achieve splendid glow. This treatment is tailored to your skin type and concerns and includes dual cleansing, steam, mechanical or chemical exfoliation, Ultrasound Skin Scrubber and/or extractions, hydrating Hydrojelly mask, relaxing massage techniques, treatment serum, and moisturizer based on the skin’s needs.",
+    duration: "1 hr",
+    price: "$128",
+  },
+  {
+    id: 2,
+    imgUrl: "/pngs/viPeelPurifywPrecisionPlus.png",
+    name: "Acne detox facial",
+    description:
+      "The Signature Facial is a deep pore cleansing facial, great for all skin types which helps to relax tension in the face, brighten, and improve the overall appearance of your skin. We customize each Signature Facial to help firm, hydrate, reduce roughness, restore its optimum texture, and achieve splendid glow. This treatment is tailored to your skin type and concerns and includes dual cleansing, steam, mechanical or chemical exfoliation, Ultrasound Skin Scrubber and/or extractions, hydrating Hydrojelly mask, relaxing massage techniques, treatment serum, and moisturizer based on the skin’s needs.",
+    duration: "1 hr 15 Min",
+    price: "$148",
+  },
+  {
+    id: 3,
+    imgUrl: "/pngs/bodyViPeelSmallArea.png",
+    name: "Gua sha Facial",
+    description:
+      "The Gua Sha Facial is focused on lifting, relaxing muscle tension, rejuvenating, healing, and inducing glow. Specific massage techniques manually and with jade stones/rollers are used in the Gua Sha treatment to invite healing into the skin, detoxify stagnant lymph, sculpt the face, firm and lift the skin, release tension, and aid in alleviating blocked sinuses and congestion.",
+    duration: "1 hr",
+    price: "$140",
+  },
+  {
+    id: 4,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "Oxygen facial",
+    description:
+      "The Oxygen Facial is great for all skin types, especially oily, congested, or breakout prone. The procedure uses pure oxygen molecules to hydrate the skin and stimulate collagen production. The facial also infuses the skin with serums that contain vitamins, antioxidants, peptides, and hyaluronic acid. Results include improved clarity, texture, and skin tone. Recommended to do 4-5 treatments for the optimal results.",
+    duration: "1 hr",
+    price: "$160",
+  },
+  {
+    id: 5,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "Hydro Facial",
+    description:
+      "HydroDermabrasion is a unique and transformative facial treatment that combines the benefits of diamond microdermabrasion and hydrodermabrasion, providing deep skin cleansing and rejuvenation, efficiently removing dead skin cells and enhancing skin texture.",
+    duration: "1 hr",
+    price: "$195",
+  },
+  {
+    id: 6,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "New glow custom facial",
+    description:
+      "The New Glow Custom Facial starts with a consultation to address skin conditions and concerns, a cleanse/steam treatment, and skin analysis. The Custom Facial will include customized exfoliation achieved by a Latic Acid Solution, Ultrasound Skin Scrubber, Microdermabrasion or HydroDermabrasion.  This one-of-a-kind glow inducing facial can include any or all of the following treatments: Diamond Microdermabrasion, LED light therapy, Microcurrent, Electroporation Therapy (MesoTherapy), RadioFrequency, HydroDermabrasion, Oxygen Infusion, Ultrasound, and High Frequency.",
+    duration: "1 hr",
+    price: "$280",
+  },
+  {
+    id: 7,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "RF Tightening Facial (Face)",
+    description:
+      "The Non-Invasive Fractional Radio Frequency Facial uses gentle electrical current that helps improve skin tone, production of collagen and elastin. This new level of anti-aging technology helps treat wrinkles, photo-damaged skin, post-acne scars and stretchmarks without any pain or downtime.",
+    duration: "1 hr",
+    price: "$220",
+  },
+  {
+    id: 8,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "RF Tightening Facial (Face/Neck)",
+    description:
+      "The Non-Invasive Fractional Radio Frequency Facial uses gentle electrical current that helps improve skin tone, production of collagen and elastin. This new level of anti-aging technology helps treat wrinkles, photo-damaged skin, post-acne scars and stretchmarks without any pain or downtime.",
+    duration: "1 hr",
+    price: "$360",
+  },
+  {
+    id: 9,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "Dermaplaning (Add On)",
+    description:
+      "Our effective Microcurrent treatment can be an add-on to any of our facials to give you immediate results of a smooth, facelifted appearance. This “non-surgical facelift” treatment stimulates collagen and elastin production, giving you firmer, more youthful-looking skin.",
+    duration: "30 Min",
+    price: "$45",
+  },
+  {
+    id: 10,
+    imgUrl: "/pngs/bodyViPeelLargeArea2.png",
+    name: "Microdermabrasion (Add On)",
+    description:
+      "Microdermabrasion is an exfoliation treatment that uses a diamond-tipped wand and vacuum suction to remove dead skin and debris, while stimulating collagen production to restore elasticity. Microdermabrasion not only helps with rough textured skin, but also helps with acne scarring, dull skin, and fine lines.",
+    duration: "30 Min",
+    price: "$45",
+  },
+];
---
+++ src/components/TreatmentsContainers/Facials/constants/description.ts
@@ -0,0 +1,2 @@
+export const description =
+  "Our facial services are designed to target a wide range of skin concerns using effective, evidence-based techniques. Whether you're dealing with acne, dullness, loss of firmness, or simply seeking regular skin maintenance, our treatments combine the latest technology with proven skincare methods. We offer specialized facials including:";
