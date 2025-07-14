defe31a9090d051f1d71ee3e3e013a5ac09b96eb
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-07-14 20:54:04 +0400

    home / our Customer Review / componrnt / mobile / fixes

--- src/components/HomeComponents/CustomersReview/CustomersReview.tsx
+++ src/components/HomeComponents/CustomersReview/CustomersReview.tsx
@@ -3,31 +3,61 @@ import CustomerReviewCard from "../../../entities/CustomersReviewCard";
 import Title from "../../../shared/Title";
 
 import { descriptions } from "./constants/descriptions";
+import { descriptionsArrayMobile } from "./constants/descriptions";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+import { MEDIA_TABLET_SMALL } from "../../../constants/windowSizes";
+import CustomSwiper from "../../../features/CustomSwiper";
 
 export const CustomersReview = () => {
+  const { width } = useWindowSize();
+
+  const isMobile = width < MEDIA_TABLET_SMALL;
   return (
-    <div className={styles.customers_review}>
-      <div className={styles.title_wrapper}>
-        <Title text="OUR CUSTOMERS ABOUT US" font="fjalla-one-regular" />
-      </div>
-      <div className={styles.reviews}>
-        <div className={styles.customer_review_left}>
-          <CustomerReviewCard
-            text={descriptions.description1}
-            name="Megan W."
-          />
-          <CustomerReviewCard
-            text={descriptions.description2}
-            name="Christina K."
-          />
+    <>
+      {!isMobile ? (
+        <div className={styles.customers_review}>
+          <div className={styles.title_wrapper}>
+            <Title text="OUR CUSTOMERS ABOUT US" font="fjalla-one-regular" />
+          </div>
+          <div className={styles.reviews}>
+            <div className={styles.customer_review_left}>
+              <CustomerReviewCard
+                text={descriptions.description1}
+                name="Megan W."
+              />
+              <CustomerReviewCard
+                text={descriptions.description2}
+                name="Christina K."
+              />
+            </div>
+            <div className={styles.customer_review_right}>
+              <CustomerReviewCard
+                text={descriptions.description3}
+                name="Emily D."
+              />
+            </div>
+          </div>
         </div>
-        <div className={styles.customer_review_right}>
-          <CustomerReviewCard
-            text={descriptions.description3}
-            name="Emily D."
-          />
+      ) : (
+        <div className={styles.customers_review}>
+          <div className={styles.title_wrapper}>
+            <Title text="OUR CUSTOMERS ABOUT US" font="fjalla-one-regular" />
+          </div>
+          <div className={styles.reviews}>
+            <CustomSwiper
+              slides={descriptionsArrayMobile.map((customerReview) => (
+                <CustomerReviewCard
+                  key={customerReview.id}
+                  text={customerReview.description}
+                  name={customerReview.name}
+                />
+              ))}
+              isArrow={!isMobile}
+              bulletsClassName="bullets_experts"
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
