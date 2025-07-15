57925d053519731a04435e6328f9daba189ac262
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-07-15 23:44:30 +0400

    home / mobile / fixes

--- src/components/HomeComponents/HomeProducts/HomeProducts.tsx
+++ src/components/HomeComponents/HomeProducts/HomeProducts.tsx
@@ -2,14 +2,26 @@ import { PRODUCTS } from "../../../constants/products";
 import ProductCard from "../../../entities/ProductCard";
 import Title from "../../../shared/Title";
 import styles from "./HomeProducts.module.css";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+import { MEDIA_TABLET_SMALL } from "../../../constants/windowSizes";
+import Button from "../../../shared/Button";
+import { useState } from "react";
 
 export const HomeProducts = () => {
+  const { width } = useWindowSize();
+  const [showMore, setShowMore] = useState(false);
+  const isMobile = width < MEDIA_TABLET_SMALL;
+
   return (
     <div className={styles.homeProducts}>
       <div className={styles.titleWrapper}>
         <Title text="PRODUCTS" font="fjalla-one-regular" />
       </div>
-      <div className={styles.productsContainer}>
+      <div
+        className={
+          showMore ? styles.productsContainerVisible : styles.productsContainer
+        }
+      >
         {PRODUCTS.map((product) => (
           <ProductCard
             key={product.id}
@@ -19,6 +31,13 @@ export const HomeProducts = () => {
           />
         ))}
       </div>
+      {isMobile ? (
+        <Button
+          content={showMore ? "show less" : "show more"}
+          className="button_see_more"
+          handleClick={() => setShowMore(!showMore)}
+        />
+      ) : null}
     </div>
   );
 };
