2731c5a9b64f0a0b7921f650bcf7c195b56c7ef9
Andranik <andranik.boyajyan@bk.ru>
2025-07-24 23:22:23 +0400

    About page adaptive layout

--- src/components/AboutComponents/AboutPageBottomSide/AboutPageBottomSide.tsx
+++ src/components/AboutComponents/AboutPageBottomSide/AboutPageBottomSide.tsx
@@ -1,16 +1,26 @@
 import aboutBottomSideImg from "/pngs/aboutPageBottomSide.png";
 
-import styles from "./AboutPageBottomSide.module.css";
 import AboutBookAConsultation from "../../../features/AboutBookAConsultation";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+import { MEDIA_TABLET_SMALL } from "../../../constants/windowSizes";
+import styles from "./AboutPageBottomSide.module.css";
 
 export const AboutPageBottomSide = () => {
+  const { width } = useWindowSize();
+
+  const isMobile = width < MEDIA_TABLET_SMALL;
   return (
-    <div className={styles.aboutPageBottomSide}>
+    <div
+      className={styles.aboutPageBottomSide}
+      style={isMobile ? { backgroundImage: `url(${aboutBottomSideImg})` } : {}}
+    >
       <AboutBookAConsultation />
-      <div
-        className={styles.aboutBookAConsultationImg}
-        style={{ backgroundImage: `url(${aboutBottomSideImg})` }}
-      ></div>
+      {!isMobile && (
+        <div
+          className={styles.aboutBookAConsultationImg}
+          style={{ backgroundImage: `url(${aboutBottomSideImg})` }}
+        ></div>
+      )}
     </div>
   );
 };
