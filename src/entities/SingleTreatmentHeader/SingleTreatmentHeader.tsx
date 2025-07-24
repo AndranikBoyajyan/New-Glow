bbbfba4f853b2c8de1662c462bf9f33146ff0c34
Andranik <andranik.boyajyan@bk.ru>
2025-07-24 19:54:51 +0400

    IU fixes for adaptive view

--- src/entities/SingleTreatmentHeader/SingleTreatmentHeader.tsx
+++ src/entities/SingleTreatmentHeader/SingleTreatmentHeader.tsx
@@ -3,7 +3,11 @@ import cn from "classnames";
 import type { FC } from "react";
 import styles from "./SingleTreatmentHeader.module.css";
 import { useWindowSize } from "../../hooks/useWindowSize";
-import { MEDIA_TABLET_SMALL } from "../../constants/windowSizes";
+import {
+  MEDIA_TABLET,
+  MEDIA_TABLET_LARGE,
+  MEDIA_TABLET_SMALL,
+} from "../../constants/windowSizes";
 
 interface SingleTreatmentHeaderProps {
   title: string;
@@ -16,18 +20,27 @@ export const SingleTreatmentHeader: FC<SingleTreatmentHeaderProps> = ({
 }) => {
   const { width } = useWindowSize();
 
+  const isTabletLarge = width <= MEDIA_TABLET_LARGE;
+  const isTabletSmall = width <= MEDIA_TABLET;
   const isMobile = width < MEDIA_TABLET_SMALL;
+
+  const getTitleClassName = () => {
+    if (isTabletLarge) return "title_h1_72";
+    if (isTabletSmall) return "title_h1_60";
+    if (isMobile) return "title_h1_36";
+
+    return "title_h1_86";
+  };
   return (
     <div
-      className={cn(
-        styles.singleTreatmentHeader,
-        styles.singleTreatmentHeaderBackGroundImg
-      )}
+      className={cn(styles.singleTreatmentHeader, {
+        [styles.singleTreatmentHeaderBackGroundImg]: !isMobile,
+      })}
     >
       <div className={styles.singleTreatmentHeaderContent}>
         {!isMobile && (
           <div className={styles.singleTreatmentHeaderTitle}>
-            <Title text={title} className={"title_h1_86"} isH1 />
+            <Title text={title} className={getTitleClassName()} isH1 />
           </div>
         )}
         <div
